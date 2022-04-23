2d9b128ecf388f2af4ae9c4ec8137ca190b50e15
alijawad1511 <jawad.bukhari1511@gmail.com>
2022-04-24 00:13:10 +0500

    onClick Background change added

--- app/src/main/java/com/example/imagegallery2/ImageFragment.java
+++ app/src/main/java/com/example/imagegallery2/ImageFragment.java
@@ -1,18 +1,42 @@
 package com.example.imagegallery2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ImageFragment extends Fragment {
+
+    LinearLayout parent;
+    boolean whiteFlag = false;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.image_fragment,container,false);
+        View view = inflater.inflate(R.layout.image_fragment, container, false);
+        parent = view.findViewById(R.id.fragmentParent);
+
+        parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(whiteFlag){
+                    parent.setBackgroundColor(Color.BLACK);
+                    whiteFlag = false;
+                }else{
+                    parent.setBackgroundColor(Color.WHITE);
+                    whiteFlag = true;
+                }
+
+            }
+        });
+        return view;
     }
 }
